a64ef8724c77a9185b8409591e2adb7a2cf5361d
Rahif M <rahifmanjatha372@gmail.com>
2022-10-22 23:24:27 +0530

    Update bmake.sh

--- bmake.sh
+++ bmake.sh
@@ -5,5 +5,5 @@ git remote add m https://github.com/RahifM/build
 #git fetch aponlab 11.x --depth=4
 #git cherry-pick 66a946ffba5ca3ede3226386fe34c510a8ed3579 && git cherry-pick 8d1e4b98564a7ba65c2d8d357e83ba7d4d337940
 git checkout -b auto
-git push m
+git push m HEAD
 echo "Done :)"
