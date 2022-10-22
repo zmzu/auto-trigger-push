44751f19059feb974edbdccad952c942d780a5ee
Rahif M <rahifmanjatha372@gmail.com>
2022-10-22 22:36:12 +0530

    Update config.sh

--- config.sh
+++ config.sh
@@ -4,3 +4,4 @@ git config --global user.email "rahifmanjatha372@gmail.com"
 git config --global core.editor vim
 git config --global credential.helper store
 echo "https://rahifm:$password@github.com" > ~/.git-credentials
+echo "TEST2"
