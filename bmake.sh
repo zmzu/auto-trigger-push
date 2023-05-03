a457901fc00aecc42badecf652e96093eaa1691c
RahifM <rahifmanjatha372@gmail.com>
2023-05-03 22:57:37 +0530

    test 20230503-2257

--- bmake.sh
+++ bmake.sh
@@ -1,7 +1,5 @@
-git clone https://github.com/LineageOS/android_build -b lineage-18.1 make
-cd make
-git remote add m https://github.com/RahifM/build
-wget https://raw.githubusercontent.com/RahifM/repo_update/lineage-18.1-patches/relkey.patch && git apply relkey.patch
-git commit -a -m "relkey"
-git push -f m HEAD
+git clone https://github.com/RahifM/repo_update -b test --single-branch test
+cd test
+git c --allow-empty -a -m "test $(date +'%Y%m%d-%H%M')"
+git push -f origin HEAD
 echo "Done :)"
