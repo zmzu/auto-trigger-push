d8d7680acab104d0079e0063f14e7d6d83191f0e
RahifM <rahifmanjatha372@gmail.com>
2023-05-03 23:07:02 +0530

    test 20230503-2307

--- bmake.sh
+++ bmake.sh
@@ -1,5 +1,5 @@
 git clone https://github.com/RahifM/repo_update -b test --single-branch test
 cd test
-git c --allow-empty -a -m "test $(date +'%Y%m%d-%H%M')"
+git commit --allow-empty -a -m "test $(date +'%Y%m%d-%H%M')"
 git push -f origin HEAD
 echo "Done :)"
