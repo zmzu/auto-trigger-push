0cccc2ec2553ead228fe53f40ce82818e182825b
Rahif M <rahifmanjatha372@gmail.com>
2023-05-05 10:47:08 +0530

    Update

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
-git clone https://github.com/RahifM/repo_update -b test --single-branch test
-cd test
+git clone https://github.com/ROM-builders/temporary -b beryllium-LineageOS-lineage-18.1-rahifm --single-branch rb
+cd rb
 git commit --allow-empty -a -m "test $(date +'%Y%m%d-%H%M')"
 git push -f origin HEAD
 echo "Done :)"
