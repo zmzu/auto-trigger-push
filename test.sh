a046855b281c2d18fca6959f0d31db216e5ce2cf
Rahif M <rahifmanjatha372@gmail.com>
2023-05-20 18:35:43 +0530

    Update test.sh [skip ci]

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 export TZ='Asia/Kolkata' && date
 git clone https://github.com/ROM-builders/temporary -b beryllium-LineageOS-lineage-18.1-rahifm --single-branch rb
 cd rb
-git commit --allow-empty -a -m "trigger $(date +'%Y%m%d-%H%M') [skip "
+git commit --allow-empty -a -m "trigger $(date +'%Y%m%d-%H%M')"
 git push -f origin HEAD
 echo "Done :)"
