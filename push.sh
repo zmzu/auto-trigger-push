c3208a5214bcf1373d28486734b24a381bd3fa66
zmzu <134157207+zmzu@users.noreply.github.com>
2023-05-23 17:09:47 +0530

    Update push.sh [skip ci]

--- push.sh
+++ push.sh
@@ -1,6 +1,6 @@
 export TZ='Asia/Kolkata' && date
 git clone https://github.com/zmzu/temporary -b beryllium-LineageOS-lineage-18.1-zmzu --single-branch rb
 cd rb
-git commit --allow-empty -a -m "trigger $(date +'%Y%m%d-%H%M')"
+git commit --allow-empty -a -m "trigger $(date +'%Y%m%d-%H%M') [skip ci]"
 git push -f origin HEAD
 echo "Done :)"
