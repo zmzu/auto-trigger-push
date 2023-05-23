a200035c154269036f217b8ade6dc2adbce00b85
zmzu <134157207+zmzu@users.noreply.github.com>
2023-05-23 16:33:07 +0530

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,5 +1,5 @@
-git config --global user.name "rahifm"
-git config --global user.email "rahifmanjatha372@gmail.com"
+git config --global user.name "zmzu"
+git config --global user.email "rahifmanjatha@yahoo.com"
 git config --global core.editor vim
 git config --global credential.helper store
-echo "https://rahifm:$password@github.com" > ~/.git-credentials
+echo "https://zmzu:$password@github.com" > ~/.git-credentials
