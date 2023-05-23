b93591726336e888fd3a1007b3b75cc7af02f857
zmzu <134157207+zmzu@users.noreply.github.com>
2023-05-23 16:35:27 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 export TZ='Asia/Kolkata' && date
-git clone https://github.com/zmzu/temporary -b beryllium-LineageOS-lineage-18.1-zmzu --single-branc rb
+git clone https://github.com/zmzu/temporary -b beryllium-LineageOS-lineage-18.1-zmzu --single-branch rb
 cd rb
 git commit --allow-empty -a -m "trigger $(date +'%Y%m%d-%H%M')"
 git push -f origin HEAD
